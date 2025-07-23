72a04f0d11d9edce6f596ddcf0e1e5256a990210
fromMarionneGANNAVI <frommarionnegannavi@gmail.com>
2025-07-23 18:04:11 -0400

    Inline JS and remove STATICFILES_DIRS to fix Render static issues

--- EloqAI/settings.py
+++ EloqAI/settings.py
@@ -123,13 +123,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
-
-# Default primary key field type
+# Default primary key field ype
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
