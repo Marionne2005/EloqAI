baf9805bdb16388f956c93651668e69c6d85d298
fromMarionneGANNAVI <frommarionnegannavi@gmail.com>
2025-07-23 17:18:04 -0400

    Add root URL path to dashboard_view to fix 404 on /

--- Eloq_app/urls_app.py
+++ Eloq_app/urls_app.py
@@ -7,7 +7,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("register/", register_view, name='register'),
+    path("", register_view, name='register'),
     path("login/", login_view, name='login'),
     path("logout/", logout_view, name='logout'),
     path("home/", dashboard_view, name='home'),
