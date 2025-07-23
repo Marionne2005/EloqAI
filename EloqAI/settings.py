79d1d10ca4d08bffbe84702db44f2c87b81e1e96
fromMarionneGANNAVI <frommarionnegannavi@gmail.com>
2025-07-23 17:10:54 -0400

    Fix: added ALLOWED_HOSTS for deployment

--- EloqAI/settings.py
+++ EloqAI/settings.py
@@ -28,7 +28,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',"eloqai-prod-y96x.onrender.com"]
 
 
 # Application definition
